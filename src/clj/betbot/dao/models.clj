08d2ac0b80954dcf7ab4c05ec9c3c07d04e68659
Oleg Akbarov <olegakbarov@gmail.com>
2016-02-21 02:34:05 +0300

    added bets table, create bet logic & pattern matching for bot

--- src/clj/betbot/dao/models.clj
+++ src/clj/betbot/dao/models.clj
@@ -3,8 +3,15 @@
   (:require [environ.core :refer [env]]
             [clj-time.jdbc]
             [korma.db :refer [defdb postgres]]
-            [korma.core :refer [defentity pk table has-many has-one entity-fields]]
-            [betbot.util.db :as db-util]))
+            [betbot.util.db :as db-util]
+            [korma.core :refer [entity-fields
+                                many-to-many
+                                belongs-to
+                                defentity
+                                has-many
+                                has-one
+                                table
+                                pk]]))
 
 (defdb db (db-util/korma-connection-map (env :database-url)))
 
@@ -35,6 +42,9 @@
 (defentity bets
   (pk :id)
   (table :bets)
-  (has-many users)
-  (has-one events)
-  (entity-fields :result :value))
+  (belongs-to users {:fk :user_id})
+  (belongs-to events {:fk :event_id})
+  (entity-fields
+    :outcome
+    :created_at))
+
--- src/clj/betbot/server.clj
+++ src/clj/betbot/server.clj
@@ -28,5 +28,5 @@
   (if (env :dev)
     (telegram-polling/start!)
     (telegram-api/set-webhook (str (env :host) "/api/telegram/" (env :telegram-token))))
-  (scraper/launch)
+  ; (scraper/launch)
   (run-jetty app {:port port :join? false}))
